d6527479aa0f2bae00efebecfd1ed3611e57feb0
ShivangiBhargava <90143656+ShivangiBhargava@users.noreply.github.com>
2022-10-17 18:14:43 +0530

    Create Search.py

---
+++ Search.py
@@ -0,0 +1,16 @@
+from spacy.matcher import PhraseMatcher
+from scipy import spatial
+
+# method for searching keyword from the text
+def search_for_keyword(keyword, doc_obj, nlp):
+    phrase_matcher = PhraseMatcher(nlp.vocab)
+    phrase_list = [nlp(keyword)]
+    phrase_matcher.add("Text Extractor", None, *phrase_list)
+
+    matched_items = phrase_matcher(doc_obj)
+
+    matched_text = []
+    for match_id, start, end in matched_items:
+        text = nlp.vocab.strings[match_id]
+        span = doc_obj[start: end]
+        matched_text.append(span.sent.text)
