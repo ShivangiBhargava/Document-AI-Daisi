cdeddb648cbcaf084bec53ff8a05b310dde1b6ff
ShivangiBhargava <90143656+ShivangiBhargava@users.noreply.github.com>
2022-10-18 22:52:44 +0530

    Update Search.py

--- Search.py
+++ Search.py
@@ -11,13 +11,14 @@ st.write(
 **Searching through text is one of the key focus area of Machine Learning Applications in the field of Natural Language..!
 
 ### ➔ But what if we have to search for multiple keywords from a large document (100+ pages).💡
-     ➔ Also, what if we have do a contextual search (searching for similar meaning keywords) with in our document!🤔💭
+### ➔ Also, what if we have do a contextual search (searching for similar meaning keywords) with in our document!🤔💭
 
 
-     - The conventional ‘CTRL + F’ solution would either take long long hours to accomplish this task (or in case of contextual search, 
-     it will not be able to find any meaning text)..
-
-     ### That’s why this project will be very useful❤️!!
+    ##- The conventional ‘CTRL + F’ solution would either 
+    take long long hours to accomplish this task 
+    (or in case of contextual search, it will not be able to find any meaning text)..
+     
+### That’s why this project will be very useful❤️!!
 """)
 uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
 if uploaded_file is not None:
