ad6bb64838c983b8eddeefc440e6b8b8ee4f32e8
ShivangiBhargava <90143656+ShivangiBhargava@users.noreply.github.com>
2022-10-18 23:17:12 +0530

    Update Search.py

--- Search.py
+++ Search.py
@@ -7,20 +7,21 @@ st.markdown(
         Intelligent Document Search App is related to Natural Language Processing: 
         Intelligent Search through text using Spacy and Python.
         
- ➔ Extract useful information from text using Python and Machine Learning
+       ➔ Extract useful information from text using Python and Machine Learning
 
-**Searching through text is one of the key focus area of Machine Learning Applications in the field of Natural Language..!
+       **Searching through text is one of the key focus area of Machine Learning Applications in the field of Natural Language..!
 
-### ➔ But what if we have to search for multiple keywords from a large document (100+ pages).💡
-### ➔ Also, what if we have do a contextual search (searching for similar meaning keywords) with in our document!🤔💭
+       ### ➔ But what if we have to search for multiple keywords from a large document (100+ pages).💡💭
+       ### ➔ Also, what if we have do a contextual search (searching for similar meaning keywords) with in our document!🤔
 
 
-    ##- The conventional ‘CTRL + F’ solution would either 
-    take long long hours to accomplish this task 
-    (or in case of contextual search, it will not be able to find any meaning text)..
+       ##- The conventional ‘CTRL + F’ solution would either 
+       take long long hours to accomplish this task 
+       (or in case of contextual search, it will not be able to find any meaning text)..
      
-### That’s why this project will be very useful❤️!!
-""")
+       ### That’s why this project will be very useful❤️!!
+       """
+       )
 uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
 if uploaded_file is not None:
     df = extract_data(uploaded_file)
@@ -48,5 +49,5 @@ def search_for_keyword(keyword, doc_obj, nlp):
         span = doc_obj[start: end]
         matched_text.append(span.sent.text)
 st.success("Built with Streamlit😊")
-st.info(" @Shivangi on Daisi platform")
+st.info(" 🎉 @Shivangi on Daisi platform")
 st.text("By Shivangi Bhargava")
