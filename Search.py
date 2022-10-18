f1b37509b23f2f5f46baa80a40c787b189b8ad72
ShivangiBhargava <90143656+ShivangiBhargava@users.noreply.github.com>
2022-10-18 22:44:26 +0530

    Update Search.py

--- Search.py
+++ Search.py
@@ -3,24 +3,22 @@ import streamlit as st
 st.write("# Welcome to Intelligent Document Search App! 👋")
 st.write(
         """
-        Intelligent Document Search App is related to Natural Language Processing: Intelligent Search through
-        text using Spacy and Python
-        ➔ Extract useful information from text using Python and Machine Learning
+        Intelligent Document Search App is related to Natural Language Processing: 
+        Intelligent Search through text using Spacy and Python.
+        
+ ➔ Extract useful information from text using Python and Machine Learning
+
+**Searching through text is one of the key focus area of Machine Learning Applications in the field of Natural Language..!
+
+### ➔ But what if we have to search for multiple keywords from a large document (100+ pages).💡
+     ➔ Also, what if we have do a contextual search (searching for similar meaning keywords) with in our document!🤔💭
 
 
-        **Searching through text is one of the key focus
-area of Machine Learning Applications in the
-field of Natural Language..!
+ - The conventional ‘CTRL + F’ solution would either take 
+   long long hours to accomplish this task (or in case of contextual search, it 
+   will not be able to find any meaning text)..
 
-        ### ➔ But what if we have to search for multiple keywords from a large document (100+ pages).💡
-            ➔ Also, what if we have do a contextual search (searching for similar meaning keywords) with in our document!🤔💭
-
-
-        - The conventional ‘CTRL + F’ solution would either take 
-           long long hours to accomplish this task (or in case of contextual search, it 
-            will not be able to find any meaning text)
-
-        ### That’s why this project will be very useful❤️!!
+     ### That’s why this project will be very useful❤️!!
 """)
 
 from spacy.matcher import PhraseMatcher
